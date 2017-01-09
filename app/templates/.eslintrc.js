847e7dfab9dfdd0d05ba7e2724e8376bedf30642
bitliner <giovanni.gaglione@gmail.com>
2017-01-09 04:32:01 +0100

    +updated .eslintrc for the generated html-parser

--- app/templates/.eslintrc.js
+++ app/templates/.eslintrc.js
@@ -1,13 +1,17 @@
 module.exports = {
-    "extends": "google",
-    "rules": {
-        "require-jsdoc": ["error", {
-            "require": {
-                "FunctionDeclaration": false,
-                "MethodDefinition": false,
-                "ClassDeclaration": true,
-                "ArrowFunctionExpression": false
-            }
-        }]
-    }
+	"extends": "google",
+	"rules": {
+		"max-len": ["error", {
+			"ignoreStrings": true
+		}],
+		"eol-last": ["off"],
+		"require-jsdoc": ["error", {
+			"require": {
+				"FunctionDeclaration": false,
+				"MethodDefinition": false,
+				"ClassDeclaration": true,
+				"ArrowFunctionExpression": false
+			}
+		}]
+	},
 };
