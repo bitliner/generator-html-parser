c4484fde619d33e0036261a23c179ed60e6a0092
bitliner <giovanni.gaglione@gmail.com>
2017-10-30 23:05:55 +0100

    +updated to most recent version of generator

--- app/templates/_index.js
+++ app/templates/_index.js
@@ -1,72 +1,32 @@
-/* jshint node:true */
-'use strict';
+const {} = require('@bitliner/parsly');
 
-const cheerio = require('cheerio');
-const DomainUtils = require('@bitliner/domain-utils');
-const IntervalParser = require('@bitliner/interval-parser');
-const DateParser = require('@bitliner/date-parser');
-const wae = require('web-auto-extractor').default;
+const {list, rating, text, domain, country, language, date, html, cheerio} = require('@bitliner/parsly');
 
-/**
- * [exports description]
- * @type {[type]}
- */
-module.exports = class {
-  constructor(opts) {}
-  parse(html, url) {
-    // var parsed = WAE().parse(sampleHTML)
 
-    const $ = cheerio.load(html);
-    let result = [];
+module.exports.parse = (htmlSource, url) => {
+  const fn = list('.review-item', {
+    rating: rating('.reviewer-score'),
+    html: html('.review-content'),
+    authorName: text('.author'),
+    subtitle: text('.review-item-header > div > h4'),
+    title: text('.product-title', '$page'),
+    pubDate: date('.review-date'),
+    channel: domain(url),
+    language: language(url),
+    country: country(url),
+    link: url,
+    linkToProductPage: url,
+    linkToSingleReview: url,
+  });
 
-    // using WAE, if WAE does not extract anything, remove this part to make it fully CSS selectors (cheerio)
-    const microdata = wae().parse(html).microdata;
-    const title = microdata.Product[0].name;
-    const reviews = microdata.Product[0].review;
-
-    // using WAE mixed with CSS selectors (cheerio)
-    $('.container-selector').each((i, $el) => {
-      let r = reviews[i];
-
-      // review
-      let review = {
-        rating: {
-          value: IntervalParser.get(r.reviewRating.ratingValue).part, // convert to range [1,5] if different range
-          total: 5,
-        },
-        pubDate: DateParser.get(r.datePublished),
-        pros: null,
-        cons: null,
-        html: r.description,
-        authorName: r.author,
-        authorLocation: $('.author', $el).text().trim() || null,
-        authorAge: $('.age', $el).text().trim() || null,
-        authorLink: $('.link', $el).attr('href') || null,
-        helpfulness: {
-          positiveVotes: parseInt($('.positiveVotes', $el).html()) || null,
-          total: parseInt($('.negativeVotes', $el).html()) + parseInt($('.positiveVotes', $el).html()),
-        },
-        link: url,
-        linkToProductPage: url,
-        linkToSingleReview: url,
-        subtitle: r.name,
-        title: title,
-        channel: DomainUtils.getChannelByUrl(url),
-        language: DomainUtils.getLanguageByUrl(url),
-        country: DomainUtils.getCountryByUrl(url),
+  const result = fn(htmlSource);
+  const endResult = result
+    .map((e) => {
+      e.rating = {
+        value: e.rating,
+        total: 5,
       };
-
-      result.push(review);
+      return e;
     });
-
-    return result;
-  }
-
-  getNextPages(html, url) {
-    throw new Error(
-      'This method does not need to be implemented or invoked'
-    );
-  }
-}
-;
-
+  return endResult;
+};
